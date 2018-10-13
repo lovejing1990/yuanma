3c40ac1d48d39cbcaa7942728193dd0a192ae82d
NL0bP <38990417+NL0bP@users.noreply.github.com>
2018-10-13 21:06:16 +0300

    Processing packages DD04 and DD03

--- ArcheAge/ArcheAge/Network/Connections/ClientConnection.cs
+++ ArcheAge/ArcheAge/Network/Connections/ClientConnection.cs
@@ -1,12 +1,10 @@
 ﻿using ArcheAge.ArcheAge.Structuring;
+using LocalCommons.Compression;
 using LocalCommons.Logging;
 using LocalCommons.Network;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
-using LocalCommons.Cryptography;
-using LocalCommons.Utilities;
 
 namespace ArcheAge.ArcheAge.Network.Connections
 {
@@ -63,65 +61,86 @@ namespace ArcheAge.ArcheAge.Network.Connections
             //reader.Offset += 1; //Undefined Random Byte
             byte seq = reader.ReadByte();
             byte header = reader.ReadByte(); //Packet Level
+            reader.Offset -= 2; //вернемся назад
+            ushort ls = reader.ReadLEUInt16(); //Packet Level+seq 
             ushort opcode = reader.ReadLEUInt16(); //Packet Opcode
             //обрабатываем пакеты от клиента
-            if (header == 0x05 && seq == 00)
+            //len  seq header opcode Тело пакета
+            //8200 00  01     0000   B10A4050184653CA705...
+            switch (ls)
             {
-                reader.Offset -= 2; //вернемся к hash, count
-                byte hash = reader.ReadByte(); //считываем hash или CRC (он не меняется)
-                byte count = reader.ReadByte(); //считываем count (шифрован, меняется)
-                //------------------------------
-                //потом switch уберем
-                switch (hash)
-                {
-                    case 0x33:
-                        opcode = 0x008E; //вход в игру5
-                        break;
-                    case 0x34:
-                        opcode = 0x0088; //пакет на релогин из лобби
-                        break;
-                    //case 0x35:
-                    //    opcode = 0x0088; //пакет на релогин из игры
-                    //    break;
-                    case 0x36:
-                        opcode = 0x008F; //вход в игру6
-                        break;
-                    case 0x37:
-                        opcode = 0x008B; //вход в игру2
-                        break;
-                    case 0x38:
-                        opcode = 0x008A; //вход в игру1
-                        break;
-                    case 0x39:
-                        opcode = 0x008C; //вход в игру3
-                        break;
-                    case 0x3F:
-                        opcode = 0x008D; //вход в игру4
-                        break;
-                        //default:
-                        //    msg = "";
+                case 0x03:
+                    byte[] compData3 = new byte[data.Length - 4];
+                    Buffer.BlockCopy(data, 4, compData3, 0, data.Length - 4); //получим тело пакета
+                    byte[] decompData3 = Сompressing.Decompress(compData3);   //разжимаем пакет
+                    Buffer.BlockCopy(decompData3, 0, data, 0, decompData3.Length); //получим тело пакета
+                    reader = new PacketReader(decompData3, 0);
+                    break;
+                case 0x04:
+                    byte[] compData4 = new byte[data.Length - 4];
+                    Buffer.BlockCopy(data, 4, compData4, 0, data.Length - 4); //получим тело пакета
+                    byte[] decompData4 = Сompressing.Decompress(compData4);   //разжимаем пакет
+                    Buffer.BlockCopy(decompData4, 0, data, 0, decompData4.Length); //получим тело пакета
+                    reader = new PacketReader(decompData4, 0);
+                    break;
+                case 0x05:
+                    reader.Offset -= 2; //вернемся к hash, count
+                    byte hash = reader.ReadByte(); //считываем hash или CRC (он не меняется)
+                    byte count = reader.ReadByte(); //считываем count (шифрован, меняется)
+                    //------------------------------
+                    //потом switch уберем
+                    switch (hash)
+                    {
+                        case 0x33:
+                            opcode = 0x008E; //вход в игру5
+                            break;
+                        case 0x34:
+                            opcode = 0x0088; //пакет на релогин из лобби
+                            break;
+                        //case 0x35:
+                        //    opcode = 0x0088; //пакет на релогин из игры
                         //    break;
-                }
-                //------------------------------
-                //здесь распаковка пакетов от клиента 
-                //здесь будет расшифровка пакета 0005
+                        case 0x36:
+                            opcode = 0x008F; //вход в игру6
+                            break;
+                        case 0x37:
+                            opcode = 0x008B; //вход в игру2
+                            break;
+                        case 0x38:
+                            opcode = 0x008A; //вход в игру1
+                            break;
+                        case 0x39:
+                            opcode = 0x008C; //вход в игру3
+                            break;
+                        case 0x3F:
+                            opcode = 0x008D; //вход в игру4
+                            break;
+                        default:
+                            break;
+                            //------------------------------
+                            //здесь распаковка пакетов от клиента 
+                            //здесь будет расшифровка пакета 0005
 
-                ////пробуем расшифровать
-                ////byte[] ciphertext = new byte[] {0x13, 0x00, 0x00, 0x05, 0x39, 0x96, 0x41, 0x57, 0x3F, 0x2C, 0xEF, 0x75, 0x3E, 0xC8, 0xC1, 0x75, 0xB5, 0xD2, 0x10, 0x43, 0x62};
-                //var pck = new CtoSDecrypt();
+                            ////пробуем расшифровать
+                            ////byte[] ciphertext = new byte[] {0x13, 0x00, 0x00, 0x05, 0x39, 0x96, 0x41, 0x57, 0x3F, 0x2C, 0xEF, 0x75, 0x3E, 0xC8, 0xC1, 0x75, 0xB5, 0xD2, 0x10, 0x43, 0x62};
+                            //var pck = new CtoSDecrypt();
 
-                //var size = BitConverter.GetBytes((short) data.Length);
-                //data = size.Concat(data).ToArray(); //объединили с Size
+                            //var size = BitConverter.GetBytes((short) data.Length);
+                            //data = size.Concat(data).ToArray(); //объединили с Size
 
-                //var ciphertext = Encrypt.CtoSEncrypt(data);
-                //Logger.Trace("EncodeXOR:      " + Utility.ByteArrayToString(ciphertext));
-                //var plaintext = pck.DecryptAes2(ciphertext);
-                //Logger.Trace("EncodeAES:      " + Utility.ByteArrayToString(plaintext));
-                ////"13 00 00 05" потеряли
-                ////"16 42|50 00 01 00|D7 94 01 00|3D A5 00 00|A4 3E A5" получили
-                ////reader.Offset += 2; //пропускаем hash&count
-                //Buffer.BlockCopy(plaintext, 2, data, 0, 2); //reader.ReadLEUInt16(); //Packet Opcode
-                //opcode = (ushort) BitConverter.ToInt16(plaintext, 2);
+                            //var ciphertext = Encrypt.CtoSEncrypt(data);
+                            //Logger.Trace("EncodeXOR:      " + Utility.ByteArrayToString(ciphertext));
+                            //var plaintext = pck.DecryptAes2(ciphertext);
+                            //Logger.Trace("EncodeAES:      " + Utility.ByteArrayToString(plaintext));
+                            ////"13 00 00 05" потеряли
+                            ////"16 42|50 00 01 00|D7 94 01 00|3D A5 00 00|A4 3E A5" получили
+                            ////reader.Offset += 2; //пропускаем hash&count
+                            //Buffer.BlockCopy(plaintext, 2, data, 0, 2); //reader.ReadLEUInt16(); //Packet Opcode
+                            //opcode = (ushort) BitConverter.ToInt16(plaintext, 2);
+                    }
+                    break;
+                default:
+                    break;
             }
 
             if (!DelegateList.ClientHandlers.ContainsKey(header))
---
+++ LocalCommons/Compression/Compression.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace LocalCommons.Compression
+{
+	public static class Сompressing
+	{
+		/// <summary>
+		/// Decompress DD04 packets
+		/// author: Atelo
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static Stream DecompressA(byte[] data)
+		{
+			var output = new MemoryStream();
+			using (var compressedStream = new MemoryStream(data))
+			using (var zipStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+			{
+				zipStream.CopyTo(output);
+				zipStream.Close();
+				output.Position = 0;
+				return output;
+			}
+		}
+		/// <summary>
+		/// Decompress DD04 packets
+		/// author: Atelo
+		/// </summary>
+		public static byte[] ReadFullyA(Stream input)
+		{
+			var buffer = new byte[16 * 1024];
+			using (var ms = new MemoryStream())
+			{
+				int read;
+				while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+				{
+					ms.Write(buffer, 0, read);
+				}
+				return ms.ToArray();
+			}
+		}
+		//======================================================================================================
+		//Функции для работы со сжатием строк
+		/// <summary>
+		/// Упаковка(архивирование) строки.
+		/// На входе — данные. На выходе — сжатая строка.
+		/// Compress DD04 packets
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static byte[] Compress(byte[] data)
+		{
+			var output = new MemoryStream();
+			using (var dstream = new DeflateStream(output, CompressionMode.Compress))
+			{
+				dstream.Write(data, 0, data.Length);
+				dstream.Close();
+			}
+
+			return output.ToArray();
+		}
+		/// <summary>
+		/// Распаковка(разархивирование) сжатой строки.
+		/// На входе — данные, предварительно сжатые предыдущей функцией. На выходе — распакованная строка.
+		/// Decompress DD04 packets
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static byte[] Decompress(byte[] data)
+		{
+			var input = new MemoryStream(data);
+			var output = new MemoryStream();
+			using (var dstream = new DeflateStream(input, CompressionMode.Decompress))
+			{
+				dstream.CopyTo(output);
+				dstream.Close();
+			}
+
+			return output.ToArray();
+		}
+	}
+}
+
